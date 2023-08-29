2962eb10b9e44ec5d66d78e55bc282385aba0ebc
suravitpop <pop.suravit29@gmail.com>
2023-08-29 18:49:28 +0700

    Fix map

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -21,7 +21,7 @@
       </v-col>
       <v-col cols="12" sm="12" md="6" class="text-center">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3874.943092522945!2d100.71623277580382!3d13.782304096585158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d640572dd17f1%3A0xfd2a833a5a493d0!2zNzEg4LiL4Lit4LiiIOC4o-C4suC4oeC4hOC4s-C5geC4q-C4hyAxNjQg4LmA4LiC4LiV4Lih4Li14LiZ4Lia4Li44Lij4Li1IOC4geC4o-C4uOC4h-C5gOC4l-C4nuC4oeC4q-C4suC4meC4hOC4oyAxMDUxMA!5e0!3m2!1sth!2sth!4v1693246346903!5m2!1sth!2sth"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3874.932994274932!2d100.71442747580383!3d13.782912796570946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d65301b6b6817%3A0x7d819828e3505325!2sGlobal%20Helpcare%20Solutions!5e0!3m2!1sen!2sth!4v1693309646722!5m2!1sen!2sth"
           width="95%"
           height="300"
           style="border:0; border-radius: 25px;"
